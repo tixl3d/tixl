d99025d4892857252c3d52df48bd34486375b8a4
newemka <newemka@gmail.com>
2025-09-13 17:09:35 +0200

    Make AudioWaveformExample more user friendly

--- Operators/examples/lib/io/audio/AudioWaveformExample.cs
+++ Operators/examples/lib/io/audio/AudioWaveformExample.cs
@@ -6,6 +6,9 @@ namespace Examples.Lib.io.audio{
         [Output(Guid = "e06de265-199e-465f-89df-d6c387e7a5f4")]
         public readonly Slot<Texture2D> ColorBuffer = new Slot<Texture2D>();
 
+        [Input(Guid = "c6d013f2-1502-485f-b683-15832b5b204e")]
+        public readonly InputSlot<bool> ShowInstructions = new InputSlot<bool>();
+
 
     }
 }
