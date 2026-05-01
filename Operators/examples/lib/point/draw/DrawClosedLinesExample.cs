009e595cd401317cb62c5572e9bd06ddc53aed47
newemka <newemka@gmail.com>
2026-05-01 19:57:07 +0200

    refactor: renamed DrawLinesAltExample -> DrawClosedLinesExample

--- Operators/examples/lib/point/draw/DrawClosedLinesExample.cs
+++ Operators/examples/lib/point/draw/DrawClosedLinesExample.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 
 namespace Examples.Lib.point.draw{
     [Guid("fc7fdcf6-8efa-4cec-82b6-cb6ce0696cc0")]
-    internal sealed class DrawLinesAltExample : Instance<DrawLinesAltExample>
+    internal sealed class DrawClosedLinesExample : Instance<DrawClosedLinesExample>
     {
         [Output(Guid = "9840db25-c425-4f3c-a163-20f535855b03")]
         public readonly Slot<Texture2D> ColorBuffer = new Slot<Texture2D>();
