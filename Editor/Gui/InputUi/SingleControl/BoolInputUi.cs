cf80bfa2d8281a5f61d1f039cfc0c7f624230861
newemka <newemka@gmail.com>
2026-09-23 20:57:00 +0200

    ui: add usage to bool parameter setting
    Default usage : checkbox
    Second usage: trigger

--- Editor/Gui/InputUi/SingleControl/BoolInputUi.cs
+++ Editor/Gui/InputUi/SingleControl/BoolInputUi.cs
@@ -1,15 +1,30 @@
-﻿using ImGuiNET;
+using ImGuiNET;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using T3.Core.Animation;
 using T3.Core.DataTypes;
 using T3.Core.Operator;
 using T3.Core.Operator.Slots;
+using T3.Editor.Gui.Input;
+using T3.Editor.Gui.Styling;
 using T3.Editor.UiModel.InputsAndTypes;
+using T3.Serialization;
 
 namespace T3.Editor.Gui.InputUi.SingleControl;
 
 public sealed class BoolInputUi : SingleControlInputUi<bool>
 {
     public override bool IsAnimatable => true;
+
+    public enum UsageType
+    {
+        CheckBox,
+        Trigger,
+    }
+    public UsageType Usage { get; private set; } = UsageType.CheckBox;
+
+    /// <summary>Whether the momentary trigger button was held during the previous frame.</summary>
+    private bool _wasHeld;
 
     public override IInputUi Clone()
     {
@@ -18,13 +33,94 @@ public sealed class BoolInputUi : SingleControlInputUi<bool>
                        InputDefinition = InputDefinition,
                        Parent = Parent,
                        PosOnCanvas = PosOnCanvas,
-                       Relevancy = Relevancy
-                   };
+                       Relevancy = Relevancy,
+                       Usage = Usage,
+        };
     }
 
     protected override bool DrawSingleEditControl(string name, ref bool value)
     {
-        return ImGui.Checkbox("##boolParam", ref value);
+        var size = Vector2.One * ImGui.GetFrameHeight();
+        switch (Usage)
+        {
+            case UsageType.CheckBox:
+             return ImGui.Checkbox("##boolParam", ref value);
+                
+            case UsageType.Trigger:
+                ImGui.Button("##boolParam", size);
+                var isHeld = ImGui.IsItemActive();
+                if (isHeld)
+                {
+                    _wasHeld = true;
+                    value = true;
+                    return true;
+                }
+
+                if (_wasHeld)
+                {
+                    _wasHeld = false;
+                    value = false;
+                    return true; 
+                }
+
+                return false;
+                
+            default:
+                return false;
+        }
+     
+    }
+
+    public override bool DrawSettings()
+    {
+        var modified = base.DrawSettings();
+        FormInputs.AddVerticalSpace();
+
+        FormInputs.DrawFieldSetHeader("Usage");
+        {
+            var tmpForRef = Usage;
+            if (FormInputs.AddEnumDropdown(ref tmpForRef, null))
+            {
+                modified = true;
+                Usage = tmpForRef;
+            }
+        }
+
+        return modified;
+    }
+
+    public override void Write(JsonTextWriter writer)
+    {
+        base.Write(writer);
+
+        if (Usage != UsageType.CheckBox)
+            writer.WriteObject(nameof(Usage), Usage.ToString());
+    }
+
+    public override void Read(JToken? inputToken)
+    {
+        if (inputToken == null)
+            return;
+
+        base.Read(inputToken);
+
+        var usageToken = inputToken[nameof(Usage)];
+        if (usageToken == null)
+            return;
+
+        var usageName = usageToken.Value<string>();
+
+        // "Default" was this enum member's name before it was renamed to "CheckBox".
+        if (usageName == "Default")
+        {
+            Usage = UsageType.CheckBox;
+            return;
+        }
+
+        if (Enum.TryParse<UsageType>(usageName, out var usageValue))
+        {
+            Usage = usageValue;
+        }
     }
 
     protected override void DrawReadOnlyControl(string name, ref bool value)
